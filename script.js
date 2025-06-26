e5fed93eca99369bd5c8ab469313f24b53a122a2
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-06-26 08:26:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputNum1 = document.querySelector("#num1");
+const inputNum2 = document.querySelector("#num2");
+const operacao = document.querySelector("#operacoes");
+const btnCalcular = document.querySelector("#btnCalcular");
+const resultado = document.querySelector("#resultado");
+
+console.log(inputNum1);
+console.log(inputNum2);
+console.log(operacao);
+console.log(btnCalcular);
+console.log(resultado);
+
+resultado.textContent = "Até a próxima quinta meus lindos!";
